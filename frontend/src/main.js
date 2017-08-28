3bbcd586e97478696a61b84e4d1ea5350e1e908c
Seah Chin Ying <scy.6char@gmail.com>
2017-08-28 22:07:55 +0800

    removed old unused comments

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,21 +5,3 @@ new Vue({
   el: '#app',
   render: h => h(App)
 })
-
-// import Vue from 'vue';
-// import Vuex from 'vuex';
-// import VueLeaflet from 'vueleaflet';
-// import App from './App.vue';
-
-// Vue.use(Vuex);
-
-// const store = new Vuex.Store({});
-
-// Vue.use(VueLeaflet.plugin,store);
-
-// new Vue({
-//   el: '#app',
-//   store,
-//   template: '<App/>',
-//   components: { App },
-// });
